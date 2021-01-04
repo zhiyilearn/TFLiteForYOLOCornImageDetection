74134931e962a09140ac36ca0c955d807f5f6bef
Zhiyi Li <zli04@vt.edu>
2021-01-04 14:50:17 -0500

    Add files via upload
    Update Activity.java

--- MainActivity.java
+++ MainActivity.java
@@ -42,11 +42,13 @@ public class MainActivity extends Activity  {
 
     // Configuration values for the prepackaged SSD model.
     private static final int TF_OD_API_INPUT_SIZE = 300;
-    // private static final int TF_OD_API_INPUT_SIZE = 640;
+    // private static final int TF_OD_API_INPUT_SIZE = 416;
     private static final boolean TF_OD_API_IS_QUANTIZED = true;
-    // private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
-    private static final String TF_OD_API_MODEL_FILE = "yolo_v3_with_metadata.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
+    // private static final String TF_OD_API_MODEL_FILE = "yolo_v3_with_metadata.tflite";
     private static final String TF_OD_API_LABELS_FILE = "labelmap.txt";
+    // private static final String TF_OD_API_LABELS_FILE = "labelmap2.txt";
+    // private static final String TF_OD_API_LABELS_FILE = "labelmap.txt";
     // Minimum detection confidence to track a detection.
     private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
     private static final boolean MAINTAIN_ASPECT = false;
@@ -63,15 +65,6 @@ public class MainActivity extends Activity  {
     image = (ImageView) findViewById(R.id.imageView1);
     image.setImageResource(R.drawable.img_0107);
 
-    //image.setVisibility(View.VISIBLE);
-    //button.setVisibility(View.VISIBLE);
-        /*BitmapFactory.Options op = new BitmapFactory.Options();
-        op.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap origin_bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img_0107, op);
-        Drawable d = new BitmapDrawable(origin_bitmap);
-        Canvas canvas = new Canvas(croppedBitmap);
-        d.draw(canvas);
-        */
         int cropSize = TF_OD_API_INPUT_SIZE;
         try {
             detector =
@@ -107,14 +100,13 @@ public class MainActivity extends Activity  {
                 Matrix cropToFrameTransform;
 
                 int cropSize = TF_OD_API_INPUT_SIZE;
-
                 Bitmap croppedbitmap = Bitmap.createScaledBitmap(origin_bitmap, cropSize, cropSize, true);
                 final List<Detector.Recognition> results = detector.recognizeImage(croppedbitmap);
+                //final List<Detector.Recognition> results = detector.recognizeImage(origin_bitmap);
                 // image.setImageBitmap(croppedbitmap);
                 // button.setVisibility(View.GONE);
 
                 cropCopyBitmap = Bitmap.createScaledBitmap(origin_bitmap, cropSize, cropSize, true);
-                // final Canvas canvas = new Canvas(croppedBitmap);
                 final Paint paint = new Paint();
                 paint.setColor(Color.RED);
                 paint.setStyle(Paint.Style.STROKE);
@@ -125,11 +117,13 @@ public class MainActivity extends Activity  {
 
                 final List<Detector.Recognition> mappedRecognitions =
                         new ArrayList<Detector.Recognition>();
+                Canvas canvas = new Canvas(cropCopyBitmap);
                 for (final Detector.Recognition result : results) {
                     final RectF location = result.getLocation();
                     Log.d("Boudning boxes",  "left: " + String.valueOf(location.left));
-                    if (location != null && result.getConfidence() >= minimumConfidence) {
-                        //canvas.drawRect(location, paint);
+                    // if (location != null && result.getConfidence() >= minimumConfidence) {
+                    if (location != null) {
+                        canvas.drawRect(location, paint);
                         cropToFrameTransform.mapRect(location);
 
                         result.setLocation(location);
@@ -139,8 +133,12 @@ public class MainActivity extends Activity  {
                 Log.d("size",  String.valueOf(mappedRecognitions.size()));
                 Log.d("size",  String.valueOf(results.size()));
 
-                image.setImageBitmap(cropCopyBitmap);
+                image = (ImageView) findViewById(R.id.imageView1);
+                Bitmap scaledBackbitmap = Bitmap.createScaledBitmap(cropCopyBitmap, origin_bitmap.getWidth(), origin_bitmap.getHeight(), true);
+                image.setImageBitmap(scaledBackbitmap);
                 button.setVisibility(View.GONE);
+
+
             }
         });
 
